a9cf8dd951f1561e18f767eea3c2afb278101828
igorgf <igfigueiredo@outlook.com>
2022-06-16 12:28:19 +0200

    FEAT: UserNotFoundException implemented

--- src/main/java/nl/igf/employeemanager/exception/UserNotFoundException.java
+++ src/main/java/nl/igf/employeemanager/exception/UserNotFoundException.java
@@ -1,2 +1,7 @@
-package nl.igf.employeemanager.exception;public class UserNotFoundException {
+package nl.igf.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
