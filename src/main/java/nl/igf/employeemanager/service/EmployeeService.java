223b00c39d0041fab50d0d5aa8ed458a64ad8fc5
igorgf <igfigueiredo@outlook.com>
2022-06-16 12:27:52 +0200

    FEAT: DB connection and Employee Service

--- src/main/java/nl/igf/employeemanager/service/EmployeeService.java
+++ src/main/java/nl/igf/employeemanager/service/EmployeeService.java
@@ -1,2 +1,42 @@
-package nl.igf.employeemanager.service;public class EmployeeService {
+package nl.igf.employeemanager.service;
+
+import nl.igf.employeemanager.exception.UserNotFoundException;
+import nl.igf.employeemanager.model.Employee;
+import nl.igf.employeemanager.repository.IEmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final IEmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(IEmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
 }
